d399648a9c6ac4fe8622fb905bbe56924f20e191
raghav <raghavmysari@gmail.com>
2017-01-31 08:12:17 -0500

    Better Impl login security via interceptors & cookies

--- app/Interceptors/routeInterceptor.js
+++ app/Interceptors/routeInterceptor.js
@@ -3,8 +3,8 @@
  */
 (function () {
 
-    var inject= ['$rootScope', '$location', '$cookies'];
-    var intercept= function($rootScope, $location, $cookies) {
+    var inject= ['$rootScope', '$location', '$cookies', '$window'];
+    var intercept= function($rootScope, $location, $cookies, $window) {
         $rootScope.$on("$routeChangeStart", function (event, next, current) {
 
             var userAuthenticated =  $cookies.get('authenticated');
@@ -12,18 +12,30 @@
             if(next.isLogin && !$rootScope.savedLocation)
                 $rootScope.savedLocation ='/welcome';
 
-            console.log(userAuthenticated)
-            alert("Not"+ "   "+ (!userAuthenticated) + "   "+ (!next.isLogin) + "   "+ ((!userAuthenticated) && (!next.isLogin)));
+            /*console.log(userAuthenticated)
+            alert("Not"+ "   "+ (!userAuthenticated) + "   "+ (!next.isLogin) + "   "+ ((!userAuthenticated) && (!next.isLogin)));*/
 
             if ((!userAuthenticated) && (!next.isLogin)) {
                 /* You can save the user's location to take him back to the same page after he has logged-in */
-                alert("Not Auth"+ "   "+ userAuthenticated);
+               // alert("Not Auth"+ "   "+ userAuthenticated);
                 $rootScope.savedLocation = $location.url();
                 $location.path('/');
             }
 
 
         })
+
+        /*$window.onbeforeunload*/
+        $window.onunload= function() {
+            alert("Cleaning")
+            clearCookies()
+        }
+
+        function clearCookies(){
+            angular.forEach($cookies, function (value, key) {
+                $cookies.remove(key);
+            });
+        }
     }
     intercept.$inject= inject;
 

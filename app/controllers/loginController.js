fd5f6c182d2f6a4e5457e0f7092c534d7864f15b
raghav <raghavmysari@gmail.com>
2017-02-01 15:29:20 -0500

    Impl good login validation

--- app/controllers/loginController.js
+++ app/controllers/loginController.js
@@ -6,8 +6,24 @@
     var inject=['$scope', '$rootScope', 'validateUser','$location', '$cookies'];
     var loginController= function ($scope, $rootScope, validateUser,$location, $cookies) {
 
+        $scope.$invalid= true;
+        $scope.userValueEntered= function () {
+
+           // alert("User");
+
+            $scope.$invalid= false;
+        }
+
+        $scope.passValueEntered= function () {
+
+           // alert("pass");
+            /*$scope.my_lgn.$invalid= false;*/
+            $scope.$invalid= false;
+        }
+
         $scope.login= function () {
             var userProfile= validateUser.getUser($scope.username);
+            $scope.$invalid= true;
             $cookies.put('username', $scope.username);
             //$rootScope.password= $scope.password;
             if(userProfile && userProfile.password===$scope.password){
@@ -15,9 +31,19 @@
                 $cookies.put('authenticated', true);
                 alert("Heeeeeeeeeeeeeeeeeeeeeeeee");
                 $rootScope.savedLocation= null;
-                //$rootScope.messages= userProfile.messages;
                 $rootScope.userProfile= userProfile;
+                $scope.my_lgn.password.$setValidity("passValid", true);
+                $scope.my_lgn.username.$setValidity("userValid", true);
                 console.log($rootScope.userProfile.messages);
+            }
+            else if(!userProfile)
+            {
+                $scope.my_lgn.username.$setValidity("userValid", false);
+                $scope.my_lgn.password.$setValidity("passValid", true);
+            }
+            else{
+                $scope.my_lgn.password.$setValidity("passValid", false);
+                $scope.my_lgn.username.$setValidity("userValid", true);
             }
         }
 
